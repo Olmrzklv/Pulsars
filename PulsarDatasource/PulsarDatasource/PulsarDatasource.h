472fa64bd192c5a5b3260bcc72ea60f8169fd897
Esma GO <esmagoktekin01@gmail.com>
2019-11-08 00:16:58 +0300

    Add PulsarDatasource framework.

---
+++ PulsarDatasource/PulsarDatasource/PulsarDatasource.h
@@ -0,0 +1,19 @@
+//
+//  PulsarDatasource.h
+//  PulsarDatasource
+//
+//  Created by EsmaGO on 11/7/19.
+//  Copyright © 2019 EsmaGO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PulsarDatasource.
+FOUNDATION_EXPORT double PulsarDatasourceVersionNumber;
+
+//! Project version string for PulsarDatasource.
+FOUNDATION_EXPORT const unsigned char PulsarDatasourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PulsarDatasource/PublicHeader.h>
+
+

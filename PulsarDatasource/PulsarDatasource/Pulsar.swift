f5ba1d611e027c76851aae051187df0ed5e8bb35
Esma GO <esmagoktekin01@gmail.com>
2019-11-18 23:25:10 +0300

    Move helper functions into an extension.

--- PulsarDatasource/PulsarDatasource/Pulsar.swift
+++ PulsarDatasource/PulsarDatasource/Pulsar.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 public class Pulsar {
+    
     var name: String!
     var RAJ: String!
     var DECJ: String
@@ -38,44 +39,46 @@ public class Pulsar {
         self.z = -R * cos(theta) * cos(phi)
     }
     
-    
+}
+
+//MARK: - Helper functions
+extension Pulsar {
     private func calculateTheta(with DECJ: String) -> Float {
-        var DEC = DECJ
-        var theta: Float = 0.0
-        
-        if DEC[DEC.startIndex] == "+" {
-            DEC = DEC.replacingOccurrences(of: "+", with: "")
-        }
-        let DECJcomponents: [String] = DEC.components(separatedBy: ":")
-        
-        for i in 0...(DECJcomponents.count - 1) {
-            if let DECJnumber = NumberFormatter().number(from: DECJcomponents[i]) {
-                let DECJFloat = DECJnumber.floatValue
-                theta = theta + abs(DECJFloat) / Float(90) * Float(90) * Float( Double.pi / 2) / powf(60, Float(i))
-            }
-        }
-        
-        if DEC[DEC.startIndex] == "-" {
-            theta = -theta
-        }
-        return theta
-    }
-    
-    private func calculatePhi(with RAJ: String) -> Float {
-        var phi: Float = 0.0
-        let RAJarray: [String] = RAJ.components(separatedBy: ":")
-        
-        for i in 0...(RAJarray.count - 1) {
-            if let RAJnumber = NumberFormatter().number(from: RAJarray[i]) {
-                let RAJfloat = RAJnumber.floatValue
-                phi = phi + abs(RAJfloat) / Float(90 / 15) * Float(Double.pi / 2) / powf(60, Float(i))
-            }
-        }
-        
-        if RAJ[RAJ.startIndex] == "-" {
-            phi = -phi
-        }
-        return phi
-    }
-    
+           var DEC = DECJ
+           var theta: Float = 0.0
+           
+           if DEC[DEC.startIndex] == "+" {
+               DEC = DEC.replacingOccurrences(of: "+", with: "")
+           }
+           let DECJcomponents: [String] = DEC.components(separatedBy: ":")
+           
+           for i in 0...(DECJcomponents.count - 1) {
+               if let DECJnumber = NumberFormatter().number(from: DECJcomponents[i]) {
+                   let DECJFloat = DECJnumber.floatValue
+                   theta = theta + abs(DECJFloat) / Float(90) * Float(90) * Float( Double.pi / 2) / powf(60, Float(i))
+               }
+           }
+           
+           if DEC[DEC.startIndex] == "-" {
+               theta = -theta
+           }
+           return theta
+       }
+       
+       private func calculatePhi(with RAJ: String) -> Float {
+           var phi: Float = 0.0
+           let RAJarray: [String] = RAJ.components(separatedBy: ":")
+           
+           for i in 0...(RAJarray.count - 1) {
+               if let RAJnumber = NumberFormatter().number(from: RAJarray[i]) {
+                   let RAJfloat = RAJnumber.floatValue
+                   phi = phi + abs(RAJfloat) / Float(90 / 15) * Float(Double.pi / 2) / powf(60, Float(i))
+               }
+           }
+           
+           if RAJ[RAJ.startIndex] == "-" {
+               phi = -phi
+           }
+           return phi
+       }
 }

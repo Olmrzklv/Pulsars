f890c75f3834bf3ec9543b65ccaff3eb3083579d
Esma GO <esmagoktekin01@gmail.com>
2019-12-31 16:54:51 +0300

    Add star image.

---
+++ SoundOfPulsars/SoundOfPulsars/Extensions/UIImage.swift
@@ -0,0 +1,15 @@
+//
+//  UIImage.swift
+//  SoundOfPulsars
+//
+//  Created by EsmaGO on 12/22/19.
+//  Copyright © 2019 EsmaGO. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    static var starMap: UIImage? {
+        return UIImage(named: "starMap")
+    }
+}
